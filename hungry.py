ad388635e909f07b8e1a1c0416b8130271d555fa
Ravi <rpowerg80@gmail.com>
2026-01-30 19:10:14 +0530

    pav bhaji

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ if hungry=="yes":
     print("eat pizza")
     print("eat burger")
     print("fries")
+    print("pav bhaji")
 else:
     thirsty = input("are you thirsty: ")
     if thirsty=="yes":
