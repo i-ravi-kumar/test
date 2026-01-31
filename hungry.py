91267fc79704a0381ed65471135322ddbb5812fb
Ravi <rpowerg80@gmail.com>
2026-01-31 06:58:48 +0530

    rice_eggs

--- hungry.py
+++ hungry.py
@@ -4,6 +4,8 @@ if hungry=="yes":
     print("eat burger")
     print("fries")
     print("pav bhaji")
+    print("eat rice")
+    print("eat eggs")
 else:
     thirsty = input("are you thirsty: ")
     if thirsty=="yes":
